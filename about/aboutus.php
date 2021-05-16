2261866f30dd66dbdc4fed86cdb8f7abff14ea8a
treeleaves30760 <treeleaves30760@gmail.com>
2021-05-16 13:36:19 +0800

    fix: a href on about us

--- about/aboutus.php
+++ about/aboutus.php
@@ -44,6 +44,11 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
 			integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
 			crossorigin="anonymous"></script>
+		<style>
+		body {
+			margin-bottom: 1000px;
+		}
+		</style>
 	</head>
 	<body class="no-sidebar is-preload">
 		<header class="header">
